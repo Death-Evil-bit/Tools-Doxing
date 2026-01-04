0f4340740a8265a25f449a430d74537f7d2d5492
Death Evil <ilham.ar0422@gmail.com>
2026-01-04 18:12:06 +0700

    Paraer.py
    Add data parsing function integrated with db_dumper.py

---
+++ Parser.py
@@ -0,0 +1,8 @@
+# extract/data_parser.py - Data Parser Module
+# Fungsi sudah terintegrasi dalam db_dumper.py
+def parse_extracted_data(raw_data):
+    """
+    Parse data yang sudah diekstrak
+    Fungsi diimplementasi dalam db_dumper.py.organize_student_data() dll
+    """
+    return "Integrated in db_dumper.py"
