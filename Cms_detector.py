96e827b09667463f248a4a58042a89a7216ce8cc
Death Evil <ilham.ar0422@gmail.com>
2026-01-04 17:54:30 +0700

    Delete Cms_detector.py

--- Cms_detector.py
+++
@@ -1,9 +0,0 @@
-# recon/cms_detector.py - CMS Detection Module
-# Fungsi sudah terintegrasi dalam school_scanner.py
-# File ini sebagai placeholder untuk struktur
-def detect_school_cms(html_content):
-    """
-    Deteksi CMS sistem sekolah
-    Fungsi ini sudah diintegrasikan dalam school_scanner.py
-    """
-    return "Integrated in school_scanner.py"
