adab48b914996e4585694d7ab0dadca824b2a942
Death Evil <ilham.ar0422@gmail.com>
2026-01-19 14:51:05 +0700

    Delete Stealth.py

--- Stealth.py
+++
@@ -1,8 +0,0 @@
-# stealth/request_spoofer.py - Request Spoofing Module
-# Fungsi sudah terintegrasi dalam school_scanner.py (random_headers)
-def spoof_request_headers():
-    """
-    Spoof request headers untuk menghindari deteksi
-    Fungsi diimplementasi dalam school_scanner.py
-    """
-    return "Integrated in school_scanner.py"
