0fa9104bc6f41c9bf3e0922029bb9aa56f76758d
Death Evil <ilham.ar0422@gmail.com>
2026-01-19 14:45:41 +0700

    Delete Analyzer.py

--- Analyzer.py
+++
@@ -1,8 +0,0 @@
-# analyze/data_analyzer.py - Data Analysis Module
-# Fungsi ada dalam main.py (generate_data_report) dan db_dumper.py
-def analyze_extracted_data(extracted_data):
-    """
-    Analisis data yang sudah diekstrak
-    Fungsi ada di main.py.generate_data_report() dan db_dumper.py.generate_report()
-    """
-    return "Functions in main.py and db_dumper.py"
